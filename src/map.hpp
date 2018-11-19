dbae2ac598f29975df9e36653a329c294b84930e
leo <leo.kivikunnas@aalto.fi>
2018-11-19 19:27:32 +0200

    Poistin turhan funkkarin getColor()

--- src/map.hpp
+++ src/map.hpp
@@ -7,7 +7,6 @@ public:
     Map(std::vector<std::vector<int> > map);
 
     std::vector<std::vector<int> > getMatrix() const;
-    int getColor(int x, int y) const;
     void setTile (int x, int y, int color); 
 	int getTile(int x, int y) const;
 protected:
