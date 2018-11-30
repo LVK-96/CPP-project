54a8accc8ff0c3b8eb2e0d9e3d5f46f2e6bcbade
Ella Eilola <ella.eilola@aalto.fi>
2018-11-30 11:47:00 +0200

    added class for startmenu state

---
+++ src/GameStates/startmenu.hpp
@@ -0,0 +1,17 @@
+#include <SFML/Graphics>
+
+#include "state.hpp"
+
+class StartMenu : public State {
+    public:
+        StartMenu(GUIWindow *guiWindow);
+
+        virtual void draw(const float dt); //changes the view that will be drawn to window (defined in guiwindow class)
+        virtual void update(const float dt);
+        virtual void handleInput(); // window event loop, close window or call startGame
+
+        void startGame(); // add game to the state stack
+
+    private:
+        sf::View view_;
+};
