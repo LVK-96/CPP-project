c72fd5339daae438999884344e7219706b167b76
Ella Eilola <ella.eilola@aalto.fi>
2018-11-30 11:45:39 +0200

    added class for window object

---
+++ src/GameStates/guiwindow.hpp
@@ -0,0 +1,25 @@
+#include <stack>
+#include <SFML/Graphics>
+
+class State;
+
+class GUIWindow {
+    public:
+        GUIWindow();
+        ~GUIWindow();
+
+        void pushState(State *state); //add new state to stack
+        void popState(); //remove state from states
+        void changeState(State *state); //change state on window
+        State *peekState(); //returns the state on top of the stack
+        void gameLoop(); //calls input handlers, draws window
+
+        std::stack<State*> states_;
+        sf::RenderWindow window_;
+};
+
+/* In tilemaching.cpp/main: 
+** GUIwindow guiWindow;
+** guiWindow.pushState(new StartMenu(&guiWindow)); -> add start menu to the state stack
+** gameLoop();
+*/
