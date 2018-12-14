d9e8ce6f17347c05aaa7cc38d762dbb311b49755
Kivikunnas Leo <kivikul1@iibis.org.aalto.fi>
2018-12-15 00:08:40 +0200

    fixed jaakkos mistakes

--- src/timeattack.cpp
+++ src/timeattack.cpp
@@ -4,8 +4,8 @@ TimeAttack::TimeAttack(std::string name) : name_(name), maxtime_(60){}
 
 std::string TimeAttack::getName() const {return name_;}
 
-void TimeAttack::addTime(int time){
-    maxtime_ = maxtime_ + time;
+void TimeAttack::addTime(){
+	maxtime_ = maxtime_ + 2;
 }
 
 bool TimeAttack::checkSpecialEndCondition(float timeElapsed){
